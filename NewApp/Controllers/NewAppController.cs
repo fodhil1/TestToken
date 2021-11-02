7efcd62d8330157887b0c11239c8aaddb8e116fb
fodhil1 <fodhilzidane@gmail.com>
2021-11-02 19:23:14 -0400

    first one

---
+++ NewApp/Controllers/NewAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewApp.Controllers
+{
+    public class NewAppController : Controller
+    {
+        // GET: NewApp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
